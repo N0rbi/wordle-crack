463861417914c2faf9f15d58ea24389bcdf235d4
Kis-Szabo Norbert <ksznorbi@pop-os.localdomain>
2022-09-16 19:03:56 +0200

    Added wordle simulator, with manual evaluator

---
+++ evaluators/evaluator.py
@@ -0,0 +1,37 @@
+from random import Random
+from simulator import WordleWordSimulator, CharacterStatus
+
+class EvaluatorBase:
+
+    NUM_WORDS = 10
+
+    def __init__(self, wordle_file, random_seed=1, allowed_guesses=6):
+        self.random = Random(random_seed)
+        self._words = [word.strip() for word in open(wordle_file).readlines()]
+        self.allowed_guesses = allowed_guesses
+
+    def _guess(self, previous_state):
+        """
+            To be implemented: prevoius state is None, if you enter an invalid word 
+            (on the other hand, the counter will not go up) or if it is the first round.
+            Otherwise it will be a list of CharacterStatus.
+        """
+        raise NotImplemented()
+
+    def evaluate(self):
+        correct_guesses = 0
+        for _ in range(self.NUM_WORDS):
+            simulator = WordleWordSimulator(self.random.choice(self._words))
+            previous_state = None
+            for _ in range(self.allowed_guesses):
+                guessed_word = None
+                while guessed_word not in self._words:
+                    guessed_word = self._guess(previous_state if guessed_word is None else None)
+                
+                    
+                previous_state = simulator.guess(guessed_word)
+                if previous_state == [CharacterStatus.CORRECT] * 5:
+                    correct_guesses += 1
+                    break
+        
+        return correct_guesses / self.NUM_WORDS
---
+++ evaluators/stdin_evaluator.py
@@ -0,0 +1,32 @@
+from .evaluator import EvaluatorBase
+from simulator import CharacterStatus
+from termcolor import colored
+
+class StdinEvaluator(EvaluatorBase):
+
+    def __init__(self, wordle_file, random_seed=1, allowed_guesses=6):
+        super().__init__(wordle_file, random_seed, allowed_guesses)
+        self.__previous_guess = None
+
+    def __colored_guess(self, args):
+        char, state = args
+        if state == CharacterStatus.CORRECT:
+            return colored(char, "green")
+        elif state == CharacterStatus.MISPLACED:
+            return colored(char, "yellow")
+        elif state == CharacterStatus.MISMATCH:
+            return colored(char, "grey")
+        else:
+            raise Exception()
+    
+    def _guess(self, previous_state):
+        if previous_state:
+            print("Prevoius round results: %s" % "".join(map(self.__colored_guess, zip(self.__previous_guess, previous_state))))
+        else:
+            print("Waiting for guess")
+        self.__previous_guess = input("Please enter next input:").strip()
+        return self.__previous_guess
+
+
+if __name__ == "__main__":
+    print("Accuracy: %f" % StdinEvaluator("words.txt").evaluate())
